e2771f879799fe9694485e3d5e048490aea19fd4
Luis Peña <luisfernandop1110@gmail.com>
2016-12-15 13:25:47 -0700

    Update comments.service.js

--- app/services/comments.service.js
+++ app/services/comments.service.js
@@ -1,4 +1,4 @@
 angular.module("App")
   .factory('CommentService',['$resource',function(resource){
-      return resource("http://jsonplaceholder.typicode.com/posts/:id/comments",{id: "@id"},{update: {method: "PUT"}});
+      return resource("https://jsonplaceholder.typicode.com/posts/:id/comments",{id: "@id"},{update: {method: "PUT"}});
   }]);
